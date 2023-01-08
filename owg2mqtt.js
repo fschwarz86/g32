6b56402597b2dd0b9b380c009ffe0f1cece67064
Florian Schwarz <florian.schwarz@gmail.com>
2023-01-08 14:25:29 +0100

    added MQTT connector

--- owg2mqtt.js
+++ owg2mqtt.js
@@ -44,24 +44,24 @@ client.connect(4502, 'socket.ottowildeapp.com', function() {
 client.on('connect', function() {
     console.log('G32 Connected');
     client.on('data', function(data) {
-	    var bindata = data.toString('binary');
-	    var hexdata = Buffer.from(bindata, 'ascii').toString('hex');
+        var bindata = data.toString('binary');
+        var hexdata = Buffer.from(bindata, 'ascii').toString('hex');
 
         var t1 = parseData(hexdata[12] + hexdata[13], hexdata[14] + hexdata[15]);
-	    var t2 = parseData(hexdata[16] + hexdata[17], hexdata[18] + hexdata[19]);
-	    var t3 = parseData(hexdata[20] + hexdata[21], hexdata[22] + hexdata[23]);
-	    var t4 = parseData(hexdata[24] + hexdata[25], hexdata[26] + hexdata[27]);
+        var t2 = parseData(hexdata[16] + hexdata[17], hexdata[18] + hexdata[19]);
+        var t3 = parseData(hexdata[20] + hexdata[21], hexdata[22] + hexdata[23]);
+        var t4 = parseData(hexdata[24] + hexdata[25], hexdata[26] + hexdata[27]);
 	    
         var ex1 = parseData(hexdata[28] + hexdata[29], hexdata[30] + hexdata[31]);
-	    var ex2 = parseData(hexdata[32] + hexdata[33], hexdata[34] + hexdata[35]);
-	    var ex3 = parseData(hexdata[36] + hexdata[37], hexdata[38] + hexdata[39]);
-	    var ex4 = parseData(hexdata[40] + hexdata[41], hexdata[42] + hexdata[43]);
+        var ex2 = parseData(hexdata[32] + hexdata[33], hexdata[34] + hexdata[35]);
+        var ex3 = parseData(hexdata[36] + hexdata[37], hexdata[38] + hexdata[39]);
+        var ex4 = parseData(hexdata[40] + hexdata[41], hexdata[42] + hexdata[43]);
 
         var w = ((parseInt(hexdata[44] + hexdata[45] + hexdata[46] + hexdata[47], 16) - 4192) - 10400) / 11000;
-	    
-	    var status = {'t1': t1, 't2': t2, 't3': t3, 't4': t4, 'ex1': ex1, 'ex2': ex2, 'ex3': ex3, 'ex4': ex4, 'w': w};
-	    
-	    mqttClient.publish(mqttTopic, JSON.stringify(status), {qos: 0, retain: false});
+
+        var status = {'t1': t1, 't2': t2, 't3': t3, 't4': t4, 'ex1': ex1, 'ex2': ex2, 'ex3': ex3, 'ex4': ex4, 'w': w};
+
+        mqttClient.publish(mqttTopic, JSON.stringify(status), {qos: 0, retain: false});
     });
 	    
     client.on('error', function(err) {
